79df95e1417b23fa791c89907e1b9d9ccadec82e
yxm <yxmbest@163.com>
2020-11-24 15:33:58 +0800

    feat:汉诺塔问题

--- app/src/main/java/com/yxm/customview/algorithm/algorithms/Main.kt
+++ app/src/main/java/com/yxm/customview/algorithm/algorithms/Main.kt
@@ -6,13 +6,22 @@ import com.yxm.customview.algorithm.algorithms.array.twoSum1
 /**
  * Created by Myron at 2020/11/14 21:47
  * @email yxmbest@163.com
- * @description 算法和数据结构
+ * @description
  */
 
 fun main() {
-    val array = intArrayOf(1,3,4,5,12)
-    val result = twoSum1(array,9)
-    result?.forEach {
-        println(it)
+    val array = intArrayOf(1, 3, 4, 5, 12)
+    val result = twoSum1(array, 9)
+    Hanoi(3,'a','b','c')
+}
+
+fun Hanoi(n: Int, a: Char = 'a', b: Char = 'b', c: Char = 'c') {
+
+    if (n == 1) {
+        println()
+    } else {
+        Hanoi(n - 1, a, b, c)
+
+        Hanoi(n - 1, b, c, a)
     }
 }
