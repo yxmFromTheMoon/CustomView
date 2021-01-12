8e20d67da23a3edb2d588f6e7a2c045e629f54ae
yxm <yxmbest@163.com>
2021-01-12 11:12:23 +0800

    feat:增加helloworld

---
+++ app/src/main/java/com/yxm/customview/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.yxm.customview;
+
+/**
+ * Created by yxm at 2021/1/11 11:49
+ *
+ * @email: yxmbest@163.com
+ * @description:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
